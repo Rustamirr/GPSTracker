eb219177067d5fd1974de9309d3f2b4d355c9098
Rustam IR <rolexnix@gmail.com>
2019-07-09 16:36:29 +0400

    Preferences to kotlin

--- app/src/main/java/com/udmurtenergo/gpstracker/interactor/PreferenceInteractor.kt
+++ app/src/main/java/com/udmurtenergo/gpstracker/interactor/PreferenceInteractor.kt
@@ -14,15 +14,16 @@ class PreferenceInteractor(private val context: Context) {
     fun load(): Single<Settings> {
         return Single.fromCallable {
             val sh = PreferenceManager.getDefaultSharedPreferences(context)
-            val settings = Settings()
-            settings.isBootService = sh.getBoolean(context.getString(R.string.key_boot_service), settings.isBootService)
-            settings.minSnr = Integer.valueOf(sh.getString(context.getString(R.string.key_min_snr), ""))
-            settings.minAccuracy = Integer.valueOf(sh.getString(context.getString(R.string.key_min_accuracy), ""))
-            settings.minSatellitesCount = Integer.valueOf(sh.getString(context.getString(R.string.key_min_satellites_count),""))
-            settings.updateInterval = Integer.valueOf(sh.getString(context.getString(R.string.key_gps_update_interval),""))
-            settings.smallestDisplacement = Integer.valueOf(sh.getString(context.getString(R.string.key_smallest_displacement),""))
-            settings.serverIp = sh.getString(context.getString(R.string.key_server_ip), "")
-            settings.networkUpdateInterval = Integer.valueOf(sh.getString(context.getString(R.string.key_network_update_interval),""))
+            val settings = Settings(
+                sh.getBoolean(context.getString(R.string.key_boot_service), false),
+                Integer.valueOf(sh.getString(context.getString(R.string.key_min_snr), "")!!),
+                Integer.valueOf(sh.getString(context.getString(R.string.key_min_accuracy), "")!!),
+                Integer.valueOf(sh.getString(context.getString(R.string.key_min_satellites_count),"")!!),
+                Integer.valueOf(sh.getString(context.getString(R.string.key_gps_update_interval),"")!!),
+                Integer.valueOf(sh.getString(context.getString(R.string.key_smallest_displacement),"")!!),
+                sh.getString(context.getString(R.string.key_server_ip), "")!!,
+                Integer.valueOf(sh.getString(context.getString(R.string.key_network_update_interval),"")!!)
+            )
             settings
         }
     }
--- app/src/main/java/com/udmurtenergo/gpstracker/utils/Settings.kt
+++ app/src/main/java/com/udmurtenergo/gpstracker/utils/Settings.kt
@@ -3,11 +3,11 @@ package com.udmurtenergo.gpstracker.utils
 import java.io.Serializable
 
 data class Settings(
-    var isBootService: Boolean = false,
-    var minSnr: Int = 0,
-    var minAccuracy: Int = 0,
-    var minSatellitesCount: Int = 0,
-    var updateInterval: Int = 0,
-    var smallestDisplacement: Int = 0,
-    var serverIp: String? = null,
-    var networkUpdateInterval: Int = 0) : Serializable
+    val isBootService: Boolean,
+    val minSnr: Int,
+    val minAccuracy: Int,
+    val minSatellitesCount: Int,
+    val updateInterval: Int,
+    val smallestDisplacement: Int,
+    val serverIp: String,
+    val networkUpdateInterval: Int) : Serializable
