c8541da7c98e916b359a1b7de0a257f7a96dba4d
Islamov Rustam <rislamov@65apps.com>
2020-07-21 10:00:40 +0400

    Add other files to commit

---
+++ app/src/test/java/com/udmurtenergo/gpstracker/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package com.udmurtenergo.gpstracker
+
+import android.view.View
+import android.widget.Button
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import org.junit.Test
+import java.lang.Exception
+
+data class Person(val name: String, val age: Int)
+
+class ExampleUnitTest {
+    @Test
+    fun test() {
+
+    }
+}
+
+
